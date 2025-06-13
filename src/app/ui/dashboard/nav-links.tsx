dbfaff10aaccd61297116a580dc4d2414eb9c616
Paulino Coderias <paulinopjc@gmail.com>
2025-06-13 21:13:44 +0800

    fix: removed unused icons from nav-links

--- src/app/ui/dashboard/nav-links.tsx
+++ src/app/ui/dashboard/nav-links.tsx
@@ -1,9 +1,7 @@
 'use client';
 
 import {
-  UserGroupIcon,
   HomeIcon,
-  DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
