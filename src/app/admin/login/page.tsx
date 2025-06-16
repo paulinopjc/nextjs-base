bcb7ebaa45693004ee3a4fa2659fdd1d07f9d6fe
Paulino Coderias <paulinopjc@gmail.com>
2025-06-16 21:31:57 +0800

    fix: while authenticated /admin/login redirects to /admin/dashboard

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,19 +1,20 @@
-import LoginForm from '@/components/auth/login-form';
 import { Suspense } from 'react';
- 
-export default function LoginPage() {
+import { redirect } from 'next/navigation';
+import { getSessionUser } from '@lib/auth/getSessionUser';
+import LoginPageClient from '@/components/auth/login-client';
+
+export const dynamic = 'force-dynamic';
+
+export default async function LoginPage() {
+  const user = await getSessionUser();
+
+  if (user) {
+    redirect('/admin/dashboard');
+  }
+
   return (
-    <main className="flex items-center justify-center md:h-screen">
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-          <div className="w-32 text-white md:w-36">
-            
-          </div>
-        </div>
-        <Suspense>
-          <LoginForm />
-        </Suspense>
-      </div>
-    </main>
+    <Suspense>
+      <LoginPageClient />
+    </Suspense>
   );
 }
---
+++ src/components/auth/login-client.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import LoginForm from '@/components/auth/login-form';
+
+export default function LoginPageClient() {
+  return (
+    <main className="flex items-center justify-center md:h-screen">
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+          <div className="w-32 text-white md:w-36">
+            {/* Logo or header */}
+          </div>
+        </div>
+        <LoginForm />
+      </div>
+    </main>
+  );
+}
